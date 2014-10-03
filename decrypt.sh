8d217efc8ff7ffa699835a200156c3222bbca6a7
Takanori TANIGUCHI <askme@pretendco.net>
2014-10-03 22:10:21 +0900

    init

---
+++ decrypt.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+WDIR=`pwd`
+KC="${WDIR}/FileVaultMaster.keychain"
+PASS="${WDIR}/pass.txt"
+
+if [ ! -f "$KC" ];   then echo "Not found $KC"   ; exit 1 ; fi
+if [ ! -f "$PASS" ]; then echo "Not found $PASS" ; exit 1 ; fi
+
+CSUUID=`diskutil cs list | grep Logical | tail -1 | awk '{print $4}'`
+if [ ${CSUUID:-x} = x ];then
+	echo "Humm, this Mac may not been FileVaulted."
+	echo "Pliase check it."
+	diskutil cs list
+	exit 1
+fi
+
+KCPASS=`cat "$PASS" | tr -d '\n'`
+security unlock-keychain -p "$KCPASS" "$KC"
+if [ $? -ne 0 ]; then exit 1 ; fi
+
+diskutil cs unlockVolume "$CSUUID" -recoveryKeychain "$KC"
+if [ $? -ne 0 ]; then exit 1 ; fi
+
+diskutil cs revert "$CSUUID" -recoveryKeychain "$KC"
+if [ $? -ne 0 ]; then exit 1 ; fi
+
+while true
+do
+	PROGRESS=`diskutil cs list | grep "Conversion Progress:" | awk '{print $3}'`
+	if [ ${PROGRESS:-X} = X ]; then
+		echo "Something wong. Abort."
+		diskutil cs list
+		break
+	fi
+	if [ $PROGRESS = "100%" ]; then
+		break
+	fi
+	echo "Conversion: $PROGRESS done."
+	sleep 10
+done
+
+diskutil cs revert "$CSUUID" -recoveryKeychain "$KC"
+if [ $? -ne 0 ]; then exit 1 ; fi
+MSG="`diskutil cs list`"
+echo "$MSG"
+if [ "$MSG" = "No CoreStorage logical volume groups found" ]; then
+	okReboot=YES
+else
+	diskutil cs list
+fi
+
+if [ ${okReboot:-NO} = "YES" ]; then
+	echo "OK. You can reboot now."
+	echo "Do you want to reboot now? [y/n]"
+	while true
+	do
+        	read ANS
+        	R=`echo $ANS | tr [:upper:] [:lower:]`
+	
+        	case ${R:-n} in
+        	y | yes )
+			systemsetup -setstartupdisk "`systemsetup -liststartupdisks | tail -1`"
+			sync
+			sync
+			sync
+			/sbin/reboot
+                	;;
+        	n | no )
+			CODE=0
+                	break
+                	;;
+        	* )
+                	echo "Do you want to reboot now? [y/n]"
+                	;;
+        	esac
+	done
+fi
+
+exit ${CODE:-1}
